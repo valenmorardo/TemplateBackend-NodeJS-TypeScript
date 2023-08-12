46ec4b44562abec08531da578e245c32ef1279dc
Valentin <valentin.morardo@gmail.com>
2023-08-11 22:36:16 -0300

    principal route added to index.ts | tsconfig finish

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,18 @@
 
 import express from "express";
 
+import indexRoute from '@routes/index.routes'
+
+
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
+
+app.use('/', indexRoute)
+
+
 
 app.listen(3000, () => console.log("server running port 3000"));
