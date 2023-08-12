45e53a768a0189c5f6d80d21c791a799f72372c1
Valentin <valentin.morardo@gmail.com>
2023-08-11 21:47:46 -0300

    express and @types/express added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('HolaMundo')
+// 	   /\_/\  
+//    ( o.o )
+// 	   > ^ <
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//		─────▄████▀█▄
+//		───▄█████████████████▄
+//		─▄█████.▼.▼.▼.▼.▼.▼▼▼▼
+//		▄███████▄.▲.▲▲▲▲▲▲▲▲
+//		████████████████████▀▀
+
+import express from 'express';
+
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello world' })
+})
+
+
+app.listen(3000, () => console.log('server running port 3000'))
