195d02b6044e4c6b02ff54a5a4fda201877caae9
Valentin <valentin.morardo@gmail.com>
2023-08-16 16:30:49 -0300

    config babel and tsconfig. Template finished

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,27 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current'
-          }
-        }
-      ],
-      '@babel/preset-typescript'
-    ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@config': './src/config',
-            '@controllers': './src/controllers',
-            '@routes': './src/routes'
-          }
-        }
-      ]
-    ]
-  };
+	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: 'current',
+				},
+			},
+		],
+		'@babel/preset-typescript',
+	],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				alias: {
+					'@config': './src/config',
+					'@controllers': './src/controllers',
+					'@routes': './src/routes',
+					'@models': './src/models/',
+					'@services': '.src/services/',
+				},
+			},
+		],
+	],
+};
