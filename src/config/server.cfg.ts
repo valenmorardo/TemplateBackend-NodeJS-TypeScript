d914d459f6287123b71e9fe536d4be1dd2b7593b
Valentin <valentin.morardo@gmail.com>
2023-09-11 22:48:26 -0300

    server cfg file  updated

--- src/config/server.cfg.ts
+++ src/config/server.cfg.ts
@@ -1,40 +1,43 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import express from 'express';
+import env from '@config/var-environments';
+import express, { Application } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-const server = express();
+const server: Application = express();
 
-// PARAMETROS DEL SERVER
-// cualquier peticion la converitmos en json
+server.set('port', env.PORT);
+
 server.use(bodyParser.json());
 server.use(express.json());
 server.use(cookieParser());
 // cargamos body parser que es un middleware para analizar cuerpos atravez de la url
 server.use(bodyParser.urlencoded({ extended: false }));
 // activamos el CORS para permitir las peticions AJAX y HTTP desde el front
-server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
+server.use((req: Request, res: Response, next: NextFunction) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Access-Control-Allow-Headers, Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Request-Method, Access-Control-Allow-Request-Method, Access-Control-Request-Headers,  Access-Control-Allow-Origin',
 	);
-	res.header('Access-Control-Allow-Headers', '*');
-	res.header('Access-Control-Request-Headers', '*');
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header(
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Request-Headers', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'GET, HEAD, POST, OPTIONS, PUT, DELETE',
 	);
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
 
-
-
 import router from '@routes/index.routes';
-const allRoutes = router;
+const allRoutes: express.Router = router;
 
-server.use('/', allRoutes);
+server.get('/', (req, res) => {
+	res.json('Go to /api that is the main route :)');
+});
+server.use('/api', allRoutes);
 
 export default server;
