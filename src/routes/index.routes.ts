49c39832156f3cbad864849c9365935c50ebdf1b
Valentin <valentin.morardo@gmail.com>
2023-09-11 22:49:09 -0300

    index.routes.ts updated

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,16 +1,12 @@
-import * as controller from '@controllers/controller';
-
+/* import * as controller from '@controllers/allControllers';
+ */
 import { Router } from 'express';
+
 const router = Router();
 
-router.get('/api', (req, res) => {
+router.get('/', (req, res) => {
 	res.json({ message: 'Welcome to my API' });
 });
 
-router.get('/getX', controller.getX);
-router.post('/postX', controller.postX);
-router.put('/putX', controller.putX);
-router.delete('/deleteX', controller.deleteX);
-router.patch('/patchX', controller.patchX);
 
 export default router;
